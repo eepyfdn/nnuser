95a5a374ca234a0c6c7f3b7f787bf7fbcc1cfc97
missrce <vcs@rce.run>
2024-10-10 01:12:09 +0100

    build: add NixOS/nixpkgs/nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   outputs = inputs: {};
-  
+
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     wrapper-manager = {
       url = "github:viperML/wrapper-manager";
       inputs.nixpkgs.follows = "";
