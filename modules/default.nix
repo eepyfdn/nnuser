b5330c31697fa0d6824f38398199ad3753596548
missrce <vcs@rce.run>
2024-10-10 06:34:44 +0100

    refactor: hopefully fix it

--- modules/default.nix
+++ modules/default.nix
@@ -3,19 +3,21 @@
 with lib;
 
 let
-  mkUserModule = username: { config, ... }: {
-    options.nnuser.${username} = {
+  userConfiguration = mkOption {
+    options = {
       programs = mkOption {
         type = types.submodule (import ./programs);
         default = {};
         description = "Program configurations for ${username}";
       };
     };
+    default = {};
+    description = "User configurations";
   };
 
 in {
   options.nnuser = mkOption {
-    type = types.attrsOf (types.submodule mkUserModule);
+    type = types.attrsOf (types.submodule userConfiguration);
     default = {};
     description = "nnuser configuration for users";
   };
