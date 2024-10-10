e8bf3446ea841b5d3e79a7b2d755bf4c7b94a750
missrce <vcs@rce.run>
2024-10-10 01:21:49 +0100

    refactor: build: it'd be smarter to use nixos-unstable
    NixOS-unstable is more stable compared to nixpkgs-unstable we also expect users to override this value anyway to prevent outdated packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   outputs = inputs: {};
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     wrapper-manager = {
       url = "github:viperML/wrapper-manager";
       inputs.nixpkgs.follows = "";
